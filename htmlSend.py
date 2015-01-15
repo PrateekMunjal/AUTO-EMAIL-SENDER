a8bf0be3113db7fb891af1e87c5969296acac1ec
PrateekMunjal <prateekmunjal31@gmail.com>
2015-01-16 04:03:41 +0530

    added frame

--- htmlSend.py
+++ htmlSend.py
@@ -25,9 +25,119 @@ rec_id = raw_input("Enter recievers id --> ")
 subject = raw_input("Enter subject --> ")
 body = raw_input("Enter body --> ")
 
-html_text = "<h1>Subject --> <br>"+subject+"</h1>"
-html_text += "<h2>Body --> <br>"+body+"</h2>"
 
+html_text = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<center>
+  <div style="width:600px;margin:auto">
+    <div style="background-color:#006cb5;margin-top:20px;padding:10px;color:white;font-family:helvetica;font-size:15px">
+     Subject :  
+      <i>
+        <!-- here comes the subject -->"""+subject+"""
+      </i>
+    </div>
+<div style="background-color:#006cb5;margin-top:20px;padding:10px;color:white;font-family:helvetica;font-size:15px">
+     Bleedred 
+      <i>
+        - Web Application
+      </i>
+      ;)
+    </div>
+    <div style="background-color:#f4f4f4;padding:10px;border:1pt solid #eee">
+      <a href="http://www.bleedred.org" style="text-decoration:none!important" target="_blank">
+        <div style="background-color:white;border:1pt solid #d3d3d3;padding:5px">
+          <table style="font-family:helvetica;font-size:13px;border-collapse:collapse" width="100%" border="0">
+            <tbody>
+              <tr>
+                <td colspan="3" style="font-weight:bold;line-height:20px;color:black" align="left">
+                  Visit Application
+                </td>
+              </tr>
+              <tr>
+                <td colspan="3" style="color:#006cb5;line-height:16px" align="left">
+                  <span style="font-weight:bold">
+                    Bleedred
+                  </span>
+                  (An Application to use in emergency)
+                </td>
+
+              </tr>
+              <tr>
+                <td colspan="3">
+                  <hr width="564" color="#ddd" align="center" size="1" style="margin:5px 0 2px 0">
+                </td>
+              </tr>
+              
+            </tbody>
+
+		
+          </table>
+        </div>
+      </a>
+<a href="https://www.facebook.com/Blood.App.HelpingWorld" style="text-decoration:none!important" target="_blank">
+        <div style="background-color:white;border:1pt solid #d3d3d3;padding:5px">
+          <table style="font-family:helvetica;font-size:13px;border-collapse:collapse" width="100%" border="0">
+            <tbody>
+              <tr>
+                <td colspan="3" style="font-weight:bold;line-height:20px;color:black" align="left">
+                  Visit Facebook Page
+                </td>
+              </tr>
+              <tr>
+                <td colspan="3" style="color:#006cb5;line-height:16px" align="left">
+                  <span style="font-weight:bold">
+                    Bleedred
+                  </span>
+                  (An Application to use in emergency)
+                </td>
+              </tr>
+              <tr>
+                <td colspan="3">
+                  <hr width="564" color="#ddd" align="center" size="1" style="margin:5px 0 2px 0">
+                </td>
+              </tr>
+              
+            </tbody>
+		
+          </table>
+        </div>
+<div style="background-color:white;border:1pt solid #d3d3d3;padding:5px">
+          <table style="font-family:helvetica;font-size:13px;border-collapse:collapse" width="100%" border="0">
+            <tbody>
+              <tr>
+                <td colspan="3" style="font-weight:bold;line-height:20px;color:black" align="left">
+                  Body
+                </td>
+              </tr>
+              <tr>
+                <td colspan="3" style="color:#006cb5;line-height:16px" align="left">
+                  <span style="font-weight:bold">
+                  """+body+"""
+                    <!-- here comes the body -->
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td colspan="3">
+                  <hr width="564" color="#ddd" align="center" size="1" style="margin:5px 0 2px 0">
+                </td>
+              </tr>
+              
+            </tbody>
+		
+          </table>
+        </div>
+      </a>
+    </div>
+  </div>
+</center>
+</body>
+</html>
+"""
 msg['Subject'] = subject
 msg['From'] = from_id
 msg['To'] = rec_id
